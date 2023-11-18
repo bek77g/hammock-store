a5c1e8ad87c1a848cd2a6f9251038b1cb6c3d75b
bek77g <d.zheenkul@gmail.com>
2023-11-18 20:00:04 +0600

    add: toast container styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ function App() {
   return (
     <>
       <Routers />
-      <ToastContainer />
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
     </>
   );
 }
