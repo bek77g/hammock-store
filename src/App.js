563f4140f98a9ac82ac94669ef6118d354c07266
bek77g <d.zheenkul@gmail.com>
2023-11-18 19:24:51 +0600

    remove: example heading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import Routers from './Routers/Routers';
 
 function App() {
-  return (
-    <div>
-      <h1>Hammock</h1>
-      <Routers />
-    </div>
-  );
+  return <Routers />;
 }
 
 export default App;
