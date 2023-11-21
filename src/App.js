4a201b96ef061d57164ad06766c576e896516766
bek77g <d.zheenkul@gmail.com>
2023-11-21 18:19:12 +0600

    init: store connect to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
 import Routers from './Routers/Routers';
+import store from 'store';
+
+const toastifyContainerConfig = {
+  position: 'top-right',
+  autoClose: 5000,
+  hideProgressBar: false,
+  newestOnTop: false,
+  closeOnClick: true,
+  rtl: false,
+  pauseOnFocusLoss: true,
+  draggable: true,
+  pauseOnHover: true,
+  theme: 'light',
+};
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Routers />
-      <ToastContainer
-        position='top-right'
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme='light'
-      />
-    </>
+      <ToastContainer {...toastifyContainerConfig} />
+    </Provider>
   );
 }
 
